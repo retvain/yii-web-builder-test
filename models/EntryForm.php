850064f4ff3176b57cf94df14b19a639d8ce014f
retvain <retvain@gmail.com>
2021-06-29 22:56:29 +0500

    make attributeLabels and form config

--- models/EntryForm.php
+++ models/EntryForm.php
@@ -11,4 +11,21 @@ class EntryForm extends Model
 
     public $name, $email, $text;
 
+    public function rules()
+    {
+        return [
+            [['name', 'email', 'text'], 'required'],
+            ['email', 'email'],
+        ];
+    }
+
+    // set labels in form
+    public function attributeLabels()
+    {
+        return [
+            'name' => 'CustomName:',
+            'email' => 'CustomEmail:',
+            'text' => 'CustomText:',
+        ];
+    }
 }
--- views/test/index.php
+++ views/test/index.php
@@ -8,12 +8,26 @@ use yii\widgets\ActiveForm;
     <h2>Page with form</h2>
     <p class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, accusantium aliquam autem deserunt dolorem ducimus earum est illum iste labore laudantium nam nemo neque nihil nulla perspiciatis reprehenderit sequi veritatis?</p>
 
-    <?php $form = ActiveForm::begin() ?>
+    <?php $form = ActiveForm::begin([
+            'id' => 'my-form',
+        'options' => [
+                'class' => 'form-horizontal',
+        ],
+        'fieldConfig' => [
+            'template' => "{label} \n <div class='col-md-5'>{input} </div> \n <div class='col-md-5'>{hint}</div> \n <div class='col-md-5'>{error}</div>",
+            'labelOptions' => ['class' => 'col-md-2 control-label'],
+        ]
+    ]) ?>
 
     <?php
-    echo $form->field($model, 'name');
-    echo $form->field($model, 'email');
-    echo $form->field($model, 'text')->textarea();
+    echo $form->field($model, 'name')->textInput(['placeholder' => 'Insert name']);
+
+    echo $form->field($model, 'email')->input('email', ['placeholder' => 'Insert Email']);
+
+    echo $form->field($model, 'text')->textarea([
+            'placeholder' => 'Insert text',
+        'rows' => '5',
+    ]);
 
     ?>
 
